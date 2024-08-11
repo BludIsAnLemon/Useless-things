ab256c4b9df873bef47e64a2bd66cd010ab75dc2
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-08-11 23:57:11 +0800

    Update frame measuring.js

--- frame measuring.js
+++ frame measuring.js
@@ -1,1 +1,58 @@
-e
+(function(Scratch) {
+  'use strict';
+  
+  let im = false;
+  let f = 0
+  
+  while (true) {
+     if (im) {
+        f += 1
+     }
+  }
+
+   class fm {
+     getInfo() {
+       return {
+         id: 'fm',
+         name: 'Extension made in phone',
+         blocks: [
+           {
+             opcode: 'sm',
+             blockType: Scratch.BlockType.COMMAND,
+             text: 'Start measuring frames'
+           },
+           {
+             opcode: 'stop',
+             blockType: Scratch.BlockType.COMMAND,
+             text: 'Stop measuring frames'
+           },
+           {
+             opcode: 'reset',
+             blockType: Scratch.BlockType.COMMAND,
+             text: 'Reset frames'
+           },
+           {
+             opcode: 'frames',
+             blockType: Scratch.BlockType.REPORTER,
+             text: 'frames measured'
+           }
+         ]
+       };
+     }
+
+     sm() {
+       im = true;
+     }
+
+     stop() {
+       im = false;
+     }
+     reset() {
+       f = 0
+     }
+     frames() {
+       return f
+     }
+   }
+
+Scratch.extensions.register(new fm());})(Scratch);
