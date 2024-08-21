c64f5fea1d28ce6a25d69cae5fe1a5e723c2fc73
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-08-21 19:33:22 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -16,15 +16,17 @@
              text: 'naughty words'
            },
            {
-             opcode: 'add',
-             blockType: Scratch.BlockType.COMMAND,
-             text: 'add [t] to naughty words',
+             opcode: 'includes',
+             blockType: Scratch.BlockType.BOOLEAN,
+             text: '[t] includes a naughty word?',
              arguments: {
                 t: {
                    type: Scratch.ArgumentType.STRING,
                    defaultValue: "naughty word"
                 }
              }
+           },
+           {
            }
          ]
        };
@@ -35,6 +37,17 @@
      add(args) {
        naughtyWords.push(args.t);
        naughtyWords = [...new Set(naughtyWords)];
+     }
+     includes(args) {
+        let found = false;
+        let t = args.t
+        for(let i = 0; i < naughtyWords.length; i++) {
+           const val = naughtyWords[i].toLowercase()
+           if (t.toLowercase().includes(val)) {
+              found = true;
+              break;
+           }
+        }
      }
    }
 
