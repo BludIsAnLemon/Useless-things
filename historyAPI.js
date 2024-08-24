3c9b6d521c27b32f035d7479b05ac660c8c28aef
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-08-24 18:23:13 +0800

    Update

---
+++ historyAPI.js
@@ -0,0 +1,23 @@
+class HistoryAPI {
+  getInfo() {
+    return {
+      id: 'history',
+      name: 'Web History API',
+      blocks: [
+        {
+          opcode: 'supported',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'api supported?'
+             }
+          }
+        }
+      ]
+    };
+  }
+
+  supported() {
+    return window.history ? true : false;
+  }
+}
+
+Scratch.extensions.register(new HistoryAPI());
